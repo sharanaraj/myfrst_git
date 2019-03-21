b607bb120db76312beac67e7b97c79d577583fa0
sharanaraj <sraj1kolkur@gmail.com>
2019-03-21 15:15:14 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular 2';
+  lname='sharanaraj';
+  // vari="yellow";
+  // isselected=false;
+  // style={
+  //   'color':'red',
+  //   'background-color':'yellow',
+  // }
+  // display():any{
+  //   alert("you clickedd me...!");
+  // }
+  // userdata='user';
+}
